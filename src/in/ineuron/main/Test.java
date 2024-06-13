e8237336fb3e22d1f8738fd9e71ae856f9cb735b
shellyjolly <shelly.jolly1@gmail.com>
2024-06-13 11:44:33 -0400

    Incorporating changes suggested by TL - Developer

--- src/in/ineuron/main/Test.java
+++ src/in/ineuron/main/Test.java
@@ -5,6 +5,9 @@ public class Test {
 	public static void name() {
 		System.out.println("main method");
 	}
+	
+	public static void main(String[] args) {
+		System.out.println("inside main!!")
 	}
 
 }
